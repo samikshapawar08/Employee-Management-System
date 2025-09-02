2752a5f93b3330810f91eac760d625aae26633a6
samiksha pawar <sameekshapawar06@gmail.com>
2025-09-02 12:37:08 +0530

    fifth commit

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,7 @@ const App = () => {
     <>
     {/* <Login /> */}
     {/* <EmployeeDashboard /> */}
-    <AdminDashboard />
+    {/* <AdminDashboard /> */}
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,15 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Authcontext from './context/Authcontext.jsx'
+import TaskContext from './context/TaskContext.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <Authcontext>
+      <TaskContext>
+        <App />
+      </TaskContext>
+    </Authcontext>
   </StrictMode>,
 )
