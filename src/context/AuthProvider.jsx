7a970b90a4f8fafd06c15cf7d7e7f122f95d5be3
samiksha pawar <sameekshapawar06@gmail.com>
2025-09-07 17:36:08 +0530

    tenth commit

--- src/context/AuthProvider.jsx
+++ src/context/AuthProvider.jsx
@@ -1,14 +1,26 @@
 import React from 'react'
 import { createContext, useState } from 'react'
 import { getLocaleStorage } from '../utils/localStorage'
+import { useEffect } from 'react'
 
 export const AuthContext = createContext()
 const AuthProvider = ({children}) => {
 
   const [userData, setuserData] = useState(null)
-  const data = getLocaleStorage()
+
+
+  useEffect(() => {
+    const {employee, admin}= getLocaleStorage()
+    setuserData({employee, admin})
+
+  
+  }, [])
+  
+  
+
+  
   return (
-    <AuthContext.Provider value={"sam"}>
+    <AuthContext.Provider value={userData}>
       {children}
     </AuthContext.Provider>
   )
