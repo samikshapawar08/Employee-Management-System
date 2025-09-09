62d573570b8acd0c249507548b344e9e225368fa
samiksha pawar <sameekshapawar06@gmail.com>
2025-09-09 08:26:54 +0530

    12th commit

--- src/App.jsx
+++ src/App.jsx
@@ -12,13 +12,24 @@ const App = () => {
 
   const [user, setUser] = useState(null)
   const authData = useContext(AuthContext)
-  console.log(authData.employees)
+
+  useEffect(()=>{
+
+    if(authData){
+      const loggedInUser = localStorage.getItem("loggedInUser")
+    }if(loggedInUser){
+      setUser(loggedInUser.role)
+    }
+  },[authData]);
+
 
   const handleLogin = (email,password)=>{
     if(email == 'admin@me.com' && password == '123'){
       setUser('admin')
-    }else if(email == 'user@me.com' && password == '123'){
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: 'admin' }))
+    }else if(authData && authData.employees.find((e)=> e.email == email && e.password == password)){
       setUser('employee')
+      localStorage.setItem("loggedInUser", JSON.stringify({ role: 'employee' }))
     }else{
       alert("Invalid credentials")
     }
